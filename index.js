f203902a324f25af2ba36019684d4754ee012fa5
Matthew Chandler <mcaz689@gmail.com>
2019-11-14 21:18:37 -0700

    testing tests

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.constants = {
+const constants = {
   DEFAULT_MESSAGE: 'Hello shredder.',
-}
+};
+exports.constants = constants;
 exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello shredder.';
-  res.status(200).send(message);
+  let message = req.query.message || req.body.message || constants.DEFAULT_MESSAGE;
+  res.status(200).send('fuck continuous deployment.');
 };
