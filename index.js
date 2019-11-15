cc33a5219d9982b9893fe78e7a9be40005cdfcc0
Matthew Chandler <mcaz689@gmail.com>
2019-11-14 20:45:18 -0700

    e2e test

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello guy.';
+  let message = req.query.message || req.body.message || 'Hello shredder.';
   res.status(200).send(message);
 };
