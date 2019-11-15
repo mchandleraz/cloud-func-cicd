a2231302bbe16345257cd4515e866bcdc5c1e3b1
Matthew Chandler <mcaz689@gmail.com>
2019-11-14 21:10:10 -0700

    add tests

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
+exports.constants = {
+  DEFAULT_MESSAGE: 'Hello shredder.',
+}
 exports.helloWorld = (req, res) => {
   let message = req.query.message || req.body.message || 'Hello shredder.';
   res.status(200).send(message);
